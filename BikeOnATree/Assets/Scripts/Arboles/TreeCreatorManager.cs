62d32ee629157ea452854ed09cc7bf19a46204b1
marcelogl <33746038+marcelogl@users.noreply.github.com>
2020-09-03 03:24:21 +0200

    Arboles y Calzzada

---
+++ BikeOnATree/Assets/Scripts/Arboles/TreeCreatorManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeCreatorManager : MonoBehaviour
+{
+    public GameObject[] gameObjectCollection;
+    public int NumMax_ByColumn = 6;
+    public float X_Initial_Position = -34f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateRandomTreesByColumn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateRandomTreesByColumn()
+    {        
+        float z_pos = 485f;
+        Random random = new Random();
+
+        for(int lado = 0; lado < 50; lado++)
+        {
+            float x_pos = X_Initial_Position;
+
+            for(int arbol = 0; arbol < NumMax_ByColumn; arbol++)
+            {
+                //System.Random rand = new System.Random(); 
+                int rnd = 0;//rand.Next(0, 6);
+
+                Vector3 position = new Vector3(x_pos, 2.4f, z_pos);
+                Instantiate(gameObjectCollection[rnd], position, gameObjectCollection[rnd].transform.rotation);
+                x_pos += 3f;
+            }
+
+            z_pos -= 13f;
+        }
+    }
+}
---
+++ BikeOnATree/Assets/Scripts/Arboles/TreeMovmentManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TreeMovmentManager : MonoBehaviour
+{
+    public int Tree_Speed = 10;
+
+    private Rigidbody _rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        //_rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_rigidBody.AddForce(Vector3.back * Tree_Speed, ForceMode.Force);
+        this.transform.Translate(Vector3.back * Time.deltaTime * Tree_Speed);
+
+        if (this.transform.position.y < 0)
+            Destroy(this.gameObject);
+    }
+}
