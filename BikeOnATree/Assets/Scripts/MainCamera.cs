549a850dbdd79ced077edeb2efa5754303e26c82
marcelogl <33746038+marcelogl@users.noreply.github.com>
2020-09-10 00:48:33 +0200

    Pruebas con adelantamiento de Vehiculas

--- BikeOnATree/Assets/Scripts/MainCamera.cs
+++ BikeOnATree/Assets/Scripts/MainCamera.cs
@@ -4,8 +4,10 @@ public class MainCamera : MonoBehaviour
 {
     public GameObject player;
     private Vector3 _offset;
-    private float _horizontalView;
     private float _turnSpeed = 90;
+
+    private bool _rightView = true;
+    private bool _leftView = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,21 @@ public class MainCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) {
-            _horizontalView = Input.GetAxis("Horizontal");
-            this.transform.Rotate(_horizontalView * Time.deltaTime * Vector3.up * _turnSpeed);
-        }
+        /*if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) {
+            _rightView = false;
+            _leftView = false;
+
+            if (Input.GetKey(KeyCode.RightArrow) && this.transform.rotation.eulerAngles.y < 100f) {
+                _leftView = true;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) && this.transform.rotation.eulerAngles.y > -100f) {
+                _rightView = true;
+            }
+
+            if (_rightView || _leftView) {
+                this.transform.Rotate(Input.GetAxis("Horizontal") * Time.deltaTime * Vector3.up * _turnSpeed);
+            }
+        }*/
+        this.transform.Rotate(Input.GetAxis("Horizontal") * Time.deltaTime * Vector3.up * _turnSpeed);
     }
 }
---
+++ BikeOnATree/Assets/Scripts/MainVehicles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainVehicles : MonoBehaviour
+{
+    public float Velociad = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ProjectProperties.Tree_Speed > 0) Velociad = ProjectProperties.Tree_Speed * 0.2f;
+
+        this.transform.Translate(Vector3.forward * Time.deltaTime * Velociad);
+
+        if (this.transform.position.z > ProjectProperties.Final_Tree_Position)
+            this.transform.position = new Vector3(-1f, 5f, 0f);
+    }
+}
